196e6892f15b918aed1c83b8cfe6476849e50242
jcmloiacono <jcmloiacono@gmail.com>
2022-08-10 20:14:36 +0200

    modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/dbz/dbz.module.ts 	new file:   src/app/dbz/main-page/main-page.component.html 	new file:   src/app/dbz/main-page/main-page.component.ts 	modified:   src/styles.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorModule } from './contador/contador.module';
 
 import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule, 
     HeroesModule, 
-    ContadorModule
+    ContadorModule, 
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
